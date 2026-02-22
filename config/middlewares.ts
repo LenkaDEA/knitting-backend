15f8353112abf50dd41d3242478e189572d84774
Elena Diatlenko <lenadea199900@gmail.com>
2026-02-22 23:30:07 +0300

    feat: add s3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,34 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+      /* Beginning of snippet */
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'storage.yandexcloud.net',
+                'knitting-s3.storage.yandexcloud.net',
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'storage.yandexcloud.net',
+                'knitting-s3.storage.yandexcloud.net',
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,30 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+    upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET_KEY'),
+          },
+          region: env('AWS_REGION'),
+          endpoint: env('AWS_ENDPOINT'),
+          forcePathStyle: true,
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
 
 export default config;
