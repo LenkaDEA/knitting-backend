a3ffc151a352654b4b43f60dcdd206b4a5182288
Elena Diatlenko <lenadea199900@gmail.com>
2026-03-17 14:26:39 +0300

    feat: add pattern controller

--- src/api/pattern/controllers/pattern.ts
+++ src/api/pattern/controllers/pattern.ts
@@ -1,7 +1,46 @@
-/**
- * pattern controller
- */
-
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::pattern.pattern');
+export default factories.createCoreController('api::pattern.pattern', ({ strapi }) => ({
+  async create(ctx) {
+    if (!ctx.is('multipart')) {
+      return super.create(ctx);
+    }
+
+    try {
+      const files = ctx.request.files;
+      const coverFile = files?.['files.cover']; 
+
+      let data = ctx.request.body.data;
+      if (typeof data === 'string') {
+        data = JSON.parse(data);
+      }
+
+      const entry = await strapi.documents('api::pattern.pattern').create({
+        data: data,
+        status: 'published'
+      });
+
+      if (coverFile) {
+        await strapi.plugin('upload').service('upload').upload({
+          data: {
+            refId: entry.id,
+            ref: 'api::pattern.pattern', 
+            field: 'cover',
+          },
+          files: coverFile,
+        });
+      }
+
+      const result = await strapi.documents('api::pattern.pattern').findOne({
+        documentId: entry.documentId,
+        populate: ['cover'],
+      });
+
+      return this.transformResponse(result);
+
+    } catch (err) {
+      console.error('Ошибка в кастомном контроллере:', err);
+      return ctx.badRequest('Internal error during record creation', { details: err.message });
+    }
+  }
+}));
